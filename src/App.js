c173e2f71f30a54372afa39a9a9ac9eb2b24243e
Ashish Ranjan <akp9801@gmail.com>
2022-12-24 22:26:51 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-let isLineT = false;
+import { useState } from "react";
 
-function swapval(){
-  isLineT = true;
-}
+// let isLineT = false;
+
+// function swapval(){
+//  isLineT = true;
+// }
 
 function App(){
 
-  // let isLineT = true;
+  let isLineT = true;
 
   const myLTdata = { textDecoration: "line-through" };
 
   return (
   <div>
     <p style={ isLineT? myLTdata : null }> This is paragraph</p>
-    <button onClick = { swapval }>Click Here</button>
+    <button onClick = { myLTdata }>Click Here</button>
   </div>)
 }
 
+function Myc(){
+  var[myvalue,myf] = useState(1);
+
+  function grepval(){
+    myf( myvalue + 1)
+    console.log(myvalue)
+  }
+  
+  return (
+  <div>
+    <h1> { myvalue }</h1>
+    <button onClick={grepval}>Click here</button>
+  </div>)
+
+}
 export default App
+export {Myc}
